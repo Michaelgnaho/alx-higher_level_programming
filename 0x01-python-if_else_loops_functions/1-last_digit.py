441f4ff8228755c804df185906b08ca30f49de0a
Michael Gnaho <gnahomichael@gmail.com>
2024-01-03 11:47:55 +0100

    added last gigit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    lastdig = number % -10
+else:
+    lastdig = number % 10
+if lastdig > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, lastdig))
+elif lastdig < 6 and lastdig != 0:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, lastdig))
+else:
+    print("Last digit of {:d} is 0 and is 0".format(number))
+

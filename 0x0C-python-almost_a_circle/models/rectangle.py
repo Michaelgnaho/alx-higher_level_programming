31c07f7bdc0ffbc995165616f25e1a75838f6869
Michael Gnaho <gnahomichael@gmail.com>
2024-04-10 10:30:21 +0100

    task 0 to 103

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -151,7 +151,7 @@ class Rectangle(Base):
             "y": self.y
         }
     def __str__(self):
-        """Return the print() and str() representation of the Rectangle."""
+        """Returns the print() and str() representation of the Rectangle."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)

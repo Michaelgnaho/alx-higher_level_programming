d9e531a4003fc62fa8a7af2f0896077177ddbc75
Michael Gnaho <gnahomichael@gmail.com>
2024-04-15 10:54:42 +0100

    update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script  takes in the name of a state
+This  takes in the name of a state
 as an argument and lists all cities of that
 state, using the database `hbtn_0e_4_usa`.
 """

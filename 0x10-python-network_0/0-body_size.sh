eefe55aeffad82742bedeaa1c99a010cbd7ac795
Michael Gnaho <gnahomichael@gmail.com>
2024-04-25 18:53:29 +0100

    task 0 to 6

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
-# This script to get the body size of a request
-curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
+#!/bin/bash
+# This gets the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c

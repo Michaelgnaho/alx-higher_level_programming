5abf97d386954fc04c160a8e3cbc4ea1d8443ae3
Michael Gnaho <gnahomichael@gmail.com>
2024-04-25 18:50:19 +0100

    task 0 to 6

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
+#!/usr/bin/bash
 # This script  gets the allowed methods in an server if availaible through OPTIONS http request
 curl -s -I -X OPTIONS "$1" | grep 'Allow:' | cut -f2- -d' '
